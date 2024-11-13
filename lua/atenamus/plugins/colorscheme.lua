a6a4c14b69431e9012fd542b5a415beb03498b24
Asif ali khan <90106208+Atenamus@users.noreply.github.com>
2024-11-13 09:31:23 +0530

    added more colorschemes

--- lua/atenamus/plugins/colorscheme.lua
+++ lua/atenamus/plugins/colorscheme.lua
@@ -1,7 +1,35 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require("rose-pine").setup({})
-	end,
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({})
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"EdenEast/nightfox.nvim",
+		priority = 1000,
+	},
+	{
+		"marko-cerovac/material.nvim",
+		priority = 1000,
+	},
+	{
+		"sainnhe/gruvbox-material",
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_foreground = "mix"
+		end,
+	},
+	{
+		"dracula/vim",
+		name = "dracula",
+		priority = 1000,
+	},
 }
